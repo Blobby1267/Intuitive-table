a4f843ad07b9cff0b71ad97a351731a1f345758a
Blobby1267 <benjaminhoskins101@gmail.com>
2025-03-31 16:35:05 +0100

    making main look nicer

--- main.py
+++ main.py
@@ -3,4 +3,7 @@ import MachineLearning as ml
 
 hg.run_hand_gesture_detection()
 result = ml.is_circle()
+
+print("\n\n---------------------------------")
 print(f"Is Circle: {result['is_circle']}, Confidence: {result['confidence']:.2f}")
+print("---------------------------------\n\n")
